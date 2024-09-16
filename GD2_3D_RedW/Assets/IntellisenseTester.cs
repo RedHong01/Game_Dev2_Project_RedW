75e0a7ae3b42a1426cbc18ebe9cfaf9f31ecb8dd
RedHong01 <zwang29@inside.artcenter.edu>
2024-09-16 11:10:39 -0700

    Week1 Intelisense

---
+++ GD2_3D_RedW/Assets/IntellisenseTester.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntellisenseTestert : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int redWang = 0;
+        redWang += 1;
+        redSwang += 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
